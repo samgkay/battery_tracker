7de7e3bebb2f7379053f840d3e25177f5bac7bdc
samgkay <68494292+samgkay@users.noreply.github.com>
2025-12-17 15:05:03 +0200

    Revert "Add system buy price backfill support"

--- sql/migrations/002_create_system_buy_price.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS system_buy_price (
-    ts timestamptz PRIMARY KEY,
-    sbp_gbp_per_mwh numeric NOT NULL,
-    ingested_at timestamptz NOT NULL DEFAULT now()
-);
--- Primary key creates an index on ts; an additional index is not added to avoid redundancy.
